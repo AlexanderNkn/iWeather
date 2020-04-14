2331101a645fabf57bb8ead3bc0fc7e4f000634b
Alexander <spresi@mail.ru>
2020-04-14 20:29:44 +0300

    current weather displayed on page. cannot change city

--- forecast/models.py
+++ forecast/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+PERIOD_CHOICES = (
+    ('H', 'Почасовой прогноз'),
+    ('D', 'Прогноз на неделю'),
+    ('W', 'Двухнедельный прогноз'),
+)
+
+class City(models.Model):
+    '''Модель описывает город и период прогноза погоды'''
+    city = models.CharField(max_length=200)
+    period = models.CharField(max_length=1, choices=PERIOD_CHOICES)
+
+    def __str__(self):
+        return self.city
--- forecast/views.py
+++ forecast/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+import requests
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    url = f'http://wttr.in/Khimki'
+    weather_parameters = {
+        'format': 2,
+        'M': ''
+    }
+    try:
+        response = requests.get(url, params=weather_parameters)
+    except requests.ConnectionError:
+        return HttpResponse('<сетевая ошибка>')
+    if response.status_code == 200:
+        return HttpResponse(response)
+    else:
+        return HttpResponse('<ошибка на сервере погоды>')
