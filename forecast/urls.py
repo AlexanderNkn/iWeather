176594969060a221da14e2133f18cb72d14c7c0d
Alexander <spresi@mail.ru>
2020-04-15 18:32:20 +0300

    response displayed on the same page as city

--- forecast/urls.py
+++ forecast/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.CityView.as_view(), name='city'),
-    path('fore/', views.fore, name='add_comment'),
+    path('', views.get_weather, name='get_weather'),
 ]
--- forecast/views.py
+++ forecast/views.py
@@ -1,23 +1,23 @@
 import requests
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import CreateView
 from .models import CityForm
 
 
-def fore(request):
+def get_weather(request):
+    form = CityForm(request.POST or None)
     url = 'http://api.openweathermap.org/data/2.5/weather?q=Москва&APPID=31e0c786808dd5e6d6a9d72af60d24d4'
-    
-    try:
-        response = requests.get(url)
-    except requests.ConnectionError:
-        return HttpResponse('<сетевая ошибка>')
-    if response.status_code == 200:
-        return render(request, 'index.html', {'response': response.json})
+    if form.is_valid():
+        count = 0
+        try:
+            response = requests.get(url)
+            count += 1
+        except requests.ConnectionError as e:
+            return HttpResponse(f'<сетевая ошибка - {e}>')
+        if response.status_code == 200:
+            return render(request, 'index.html', {'response': response.json, 'form': form})
+        else:
+            return HttpResponse(f'<ошибка на сервере погоды, код {response.status_code}>')
     else:
-        return HttpResponse('<ошибка на сервере погоды>')
-
-class CityView(CreateView):
-    form_class =  CityForm
-    success_url = "forecast" # куда переадресовать пользователя после успешной отправки формы
-    template_name = "index.html"
+        form = CityForm(request.POST or None)
+        return render(request, 'index.html', {'form': form})
